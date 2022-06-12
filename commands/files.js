273b3c55d7a999ce5b972557277d48dfa12ed071
effgenij <effgenij@gmail.com>
2022-06-12 22:52:12 +0300

    finish file operations

--- commands/files.js
+++ commands/files.js
@@ -1,15 +1,49 @@
 /*
-
 cat path_to_file
-
 add new_file_name
-
 rn path_to_file new_filename
-
 cp path_to_file path_to_new_directory
-
 mv path_to_file path_to_new_directory
-
 rm path_to_file
-
 */
+import fs from "fs";
+import { writeFile, rename, rm } from "fs/promises";
+import { parse, join } from "path";
+
+export const catFile = (path) => {
+  const readStream = fs.createReadStream(path);
+  readStream.on("data", (data) => {
+    process.stdout.write(data.toString());
+  });
+};
+
+export const addFile = async (path) => {
+  await writeFile(path, "");
+};
+
+export const renameFile = async (path, newName) => {
+  await rename(path, newName);
+};
+
+export const copy = async (path, newPath) => {
+  const parsingPath = parse(path);
+  const fileName = `${parsingPath.name}${parsingPath.ext}`;
+  const destinationPath = join(newPath, fileName);
+  const readable = fs.createReadStream(path);
+  const writable = fs.createWriteStream(destinationPath);
+  readable.pipe(writable);
+};
+
+export const remove = async (path) => {
+    await rm(path);
+};
+
+export const move = async (path, newPath) => {
+  const parsingPath = parse(path);
+  const fileName = `${parsingPath.name}${parsingPath.ext}`;
+  const destinationPath = join(newPath, fileName);
+  const readable = fs.createReadStream(path);
+  const writable = fs.createWriteStream(destinationPath);
+  readable.pipe(writable);
+  await remove(path);
+};
--- controllers/controller.js
+++ controllers/controller.js
@@ -7,7 +7,19 @@ import { OScommand } from "../commands/os.js";
 import { sayCWD } from "../outputs/cwd.js";
 import { moveUp, changeDirectory, list } from "../commands/navigation.js";
 import { calculateHash } from "../commands/hash.js";
-import { returnAbsolutPath, pathIsFile } from "../utils/path.js";
+import {
+  returnAbsolutPath,
+  pathIsFile,
+  pathIsDirectory,
+} from "../utils/path.js";
+import {
+  catFile,
+  addFile,
+  renameFile,
+  copy,
+  remove,
+  move,
+} from "../commands/files.js";
 
 let currentPath = os.homedir();
 
@@ -18,39 +30,78 @@ export const init = () => {
 
 export const fileManagerController = async (line) => {
   const inputs = parseLine(line);
-  try {
-    switch (inputs.command) {
-      case ".exit":
-        sayGoodbye();
-        process.exit();
-      case "up":
-        currentPath = moveUp(currentPath);
-        sayCWD(currentPath);
-        break;
-      case "cd":
-        const changedPath = changeDirectory(currentPath, inputs.arg1);
-        changedPath ? (currentPath = changedPath) : sayInputError();
-        sayCWD(currentPath);
-        break;
-      case "ls":
-        await list(currentPath);
-        sayCWD(currentPath);
-        break;
-      case "hash":
-        const hashPath = returnAbsolutPath(currentPath, inputs.arg1);
-        pathIsFile(hashPath) ? await calculateHash(hashPath) : sayInputError();
-        sayCWD(currentPath);
-        break;
-      case "os":
-        inputs.arg1 ? OScommand(inputs.arg1) : sayInputError();
-        sayCWD(currentPath);
-        break;
-      default:
-        sayInputError();
-        sayCWD(currentPath);
-    }
-  } catch (err) {
-    sayOperationError();
-    sayCWD(currentPath);
+  //try {
+  switch (inputs.command) {
+    case ".exit":
+      sayGoodbye();
+      process.exit();
+    case "up":
+      currentPath = moveUp(currentPath);
+      sayCWD(currentPath);
+      break;
+    case "cd":
+      const changedPath = changeDirectory(currentPath, inputs.arg1);
+      changedPath ? (currentPath = changedPath) : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "ls":
+      await list(currentPath);
+      sayCWD(currentPath);
+      break;
+    case "hash":
+      const hashPath = returnAbsolutPath(currentPath, inputs.arg1);
+      pathIsFile(hashPath) ? await calculateHash(hashPath) : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "os":
+      inputs.arg1 ? OScommand(inputs.arg1) : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "cat":
+      const catPath = returnAbsolutPath(currentPath, inputs.arg1);
+      pathIsFile(catPath) ? catFile(catPath) : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "add":
+      const addPath = returnAbsolutPath(currentPath, inputs.arg1);
+      await addFile(addPath);
+      sayCWD(currentPath);
+      break;
+    case "rn":
+      const rnOriginalPath = returnAbsolutPath(currentPath, inputs.arg1);
+      const rnPath = returnAbsolutPath(currentPath, inputs.arg2);
+      pathIsFile(rnOriginalPath)
+        ? renameFile(rnOriginalPath, rnPath)
+        : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "cp":
+      const cpOriginalPath = returnAbsolutPath(currentPath, inputs.arg1);
+      const cpPath = returnAbsolutPath(currentPath, inputs.arg2);
+      pathIsFile(cpOriginalPath) && pathIsDirectory(cpPath)
+        ? await copy(cpOriginalPath, cpPath)
+        : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "mv":
+      const mvOriginalPath = returnAbsolutPath(currentPath, inputs.arg1);
+      const mvPath = returnAbsolutPath(currentPath, inputs.arg2);
+      pathIsFile(mvOriginalPath) && pathIsDirectory(mvPath)
+        ? await move(mvOriginalPath, mvPath)
+        : sayInputError();
+      sayCWD(currentPath);
+      break;
+    case "rm":
+      const rmPath = returnAbsolutPath(currentPath, inputs.arg1);
+      pathIsFile(rmPath) ? remove(rmPath) : sayInputError();
+      sayCWD(currentPath);
+      break;
+    default:
+      sayInputError();
+      sayCWD(currentPath);
+    //  }
+    //} catch (err) {
+    //  sayOperationError();
+    //  sayCWD(currentPath);
   }
 };
